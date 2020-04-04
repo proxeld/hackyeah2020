3deef0cf302e4c1a6c735b8ef6961354f93d1ace
Maciej Urbanek <proxeld@gmail.com>
2020-04-04 18:47:42 +0200

    Signup form

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -7,6 +7,7 @@ import { AuthGuard, FlashMessageRoot } from 'components'
 import { history } from 'utils/history'
 import { store } from 'store/create-store'
 import { DashboardLayout, FormPageLayout } from 'layouts'
+import { ShieldFormPageLayout } from './layouts/FormPage';
 
 const LogIn = lazy(() => import('pages/LogIn/LogIn'))
 const SignUp = lazy(() => import('pages/SignUp/SignUp'))
@@ -46,16 +47,18 @@ export const App = () => (
               exact
               path="/login"
               render={() => (
+                <ShieldFormPageLayout title="Log In">
                   <LogIn />
+                </ShieldFormPageLayout>
               )}
             />
             <Route
               exact
               path="/signup"
               render={() => (
-                <FormPageLayout title="Sign Up">
+                <ShieldFormPageLayout title="Sign Up">
                   <SignUp />
-                </FormPageLayout>
+                </ShieldFormPageLayout>
               )}
             />
             <Route
--- resources/assets/js/pages/LogIn/LogInForm.jsx
+++ resources/assets/js/pages/LogIn/LogInForm.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Formik, Form, Field } from 'formik'
 
 import { email as emailRegex } from 'constants/regexes'
+import { Link } from 'react-router-dom'
+import { ShieldPasswordInput, ShieldTextInput } from '../../components/Forms/Inputs'
 
 const validateLogin = (values = {}) => {
   let errors = {}
@@ -19,28 +21,6 @@ const validateLogin = (values = {}) => {
   return errors
 }
 
-const TextInputLogin = ({className = '', ...props}) => {
-  return (
-    <input
-      placeholder={props.placeholder}
-      {...props.field}
-      type="text"
-      className={`form-control`}
-    />
-  )
-}
-
-const TextInputPassword = ({className = '', ...props}) => {
-  return (
-    <input
-      placeholder={props.placeholder}
-      {...props.field}
-      type="password"
-      className={`form-control`}
-    />
-  )
-}
-
 export default ({onSubmit}) => {
   return (
     <Formik
@@ -50,68 +30,53 @@ export default ({onSubmit}) => {
     >
       {props => (
         <Form>
-          <div className="kt-grid kt-grid--ver kt-grid--root kt-page" style={{height: '100vh'}}>
-            <div className="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v3 kt-login--signin">
-              <div className="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor"
-                   style={{backgroundImage: "url('../../../img/bg-3.jpg')"}}>
-                <div className="kt-grid__item kt-grid__item--fluid kt-login__wrapper">
-                  <div className="kt-login__container">
-                    <div className="kt-login__logo">
-                      <a href="#">
-                        <img src="../../../img/shield.png" alt="shield" className="img-fluid" style={{maxWidth: '200px'}} />
-                      </a>
-                    </div>
-                    <div className="kt-login__signin">
-                      <div className="kt-login__head">
-                        <h3 className="kt-login__title">Sign In To Admin Panel</h3>
-                      </div>
-                      <div className="kt-form">
-                        <div className="input-group">
-                          <Field
-                            type="text"
-                            name="email"
-                            placeholder="Email"
-                            component={TextInputLogin}
-                          />
-                        </div>
-                        <div className="input-group">
-                          <Field
-                            type="password"
-                            name="password"
-                            placeholder="Password"
-                            component={TextInputPassword}
-                          />
-                        </div>
-                        <div className="row kt-login__extra">
-                          <div className="col">
-                            <label className="kt-checkbox">
-                              <input type="checkbox" name="remember"/> Remember me
-                              <span/>
-                            </label>
-                          </div>
-                          <div className="col kt-align-right">
-                            <a href="#" className="kt-login__link">Forget Password?</a>
-                          </div>
-                        </div>
-                        <div className="kt-login__actions">
-                          <button
-                            className="btn btn-brand btn-elevate kt-login__btn-primary"
-                            type="submit"
-                          >
-                            Sign In
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="kt-login__account">
-                      <span className="kt-login__account-msg">Don't have an account yet?</span>
-                      &nbsp;&nbsp;
-                      <a href="#" id="kt_login_signup" className="kt-login__account-link">Sign Up!</a>
-                    </div>
-                  </div>
+          <div className="kt-login__signin d-block">
+            <div className="kt-login__head">
+              <h3 className="kt-login__title">Sign In To Admin Panel</h3>
+            </div>
+            <div className="kt-form">
+              <Field
+                type="text"
+                name="email"
+                placeholder="Email"
+                component={ShieldTextInput}
+              />
+              <Field
+                type="password"
+                name="password"
+                placeholder="Password"
+                component={ShieldPasswordInput}
+              />
+              <div className="row kt-login__extra">
+                <div className="col">
+                  <label className="kt-checkbox">
+                    <input type="checkbox" name="remember"/> Remember me
+                    <span/>
+                  </label>
+                </div>
+                <div className="col kt-align-right">
+                  <Link to="/forgot-password" className="kt-login__link">
+                    Forget Password?
+                  </Link>
                 </div>
               </div>
+              <div className="kt-login__actions">
+                <button
+                  className="btn btn-brand btn-elevate kt-login__btn-primary"
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              </div>
             </div>
+          </div>
+          <div className="kt-login__account">
+            <span className="kt-login__account-msg">Don't have an account yet?</span>
+            &nbsp;&nbsp;
+
+            <Link to="/signup" className="kt-login__account-link">
+              Sign Up!
+            </Link>
           </div>
         </Form>
       )}
